1ca83cfcd430c98a88903e4d6aea666f2d93fb0d
jainaldosilva <jainaldosilva@gmail.com>
2015-10-12 23:35:22 -0300

    ajustando admin

--- central/admin.py
+++ central/admin.py
@@ -6,12 +6,10 @@ class SalaAdmin(admin.ModelAdmin):
     '''
         Admin View for Sala
     '''
-    pass
-    #list_display = ('',)
+    list_display = ('nome', 'turno')
     #list_filter = ('',)
     #raw_id_fields = ('',)
     #readonly_fields = ('',)
-    #search_fields = ['']
 
 admin.site.register(Sala, SalaAdmin)
 
@@ -20,11 +18,10 @@ class FichaAdmin(admin.ModelAdmin):
     '''
         Admin View for Ficha
     '''
-    pass
-    #list_display = ('',)
-    #list_filter = ('',)
-    #raw_id_fields = ('',)
-    #readonly_fields = ('',)
-    #search_fields = ['']
+    list_display = ('n_ficha', 'crianca', 'responsavel', 'telefone', 'sala',
+                    'ativo')
+    list_filter = ('sala',)
+    search_fields = ['n_ficha', 'crianca', 'responsavel']
+
 
 admin.site.register(Ficha, FichaAdmin)
